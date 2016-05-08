221cb7234ad7c518fc9e48ad75cea6276c5fb1bc
hwclass <harderworking.class@gmail.com>
2016-05-08 14:49:02 +0300

    updated the dialect with ES2015, removed weight prop, added message method and added calculation

--- semantical.js
+++ semantical.js
@@ -1,69 +1,88 @@
-const cheerio = require('cheerio'),
-      $ = cheerio.load('<body><figure>This is the title.</figure></body>');
+/*!
+ * Semanticals. Test your HTML markup if it is semantic or not.
+ *
+ * Copyright (c) 2016 Barış Güler
+ * http://hwclass.in
+ *
+ * Licensed under MIT
+ * http://www.opensource.org/licenses/mit-license.php
+ *
+ *
+ * Launch  : May 2016
+ * Version : 1.0.0
+ * Released: May 8th, 2016
+ *
+ *
+ * Calculates the current markup if it has really semantic tags
+ */
 
-var total = 0;
+'use strict';
 
-const semanticals = {
-  article: {
-    value : 'article',
-    weight: 10
-  },
-  aside: {
-    value : 'aside',
-    weight: 10
-  },
-  details : {
-    value : 'details',
-    weight: 10
-  },
-  figcaption : {
-    value : 'figcaption',
-    weight: 10
-  },
-  figure : {
-    value : 'figure',
-    weight: 10
-  },
-  footer : {
-    value : 'footer',
-    weight: 10
-  },
-  header : {
-    value : 'header',
-    weight: 10
-  },
-  main : {
-    value : 'main',
-    weight: 10
-  },
-  mark : {
-    value : 'mark',
-    weight: 10
-  },
-  nav : {
-    value : 'nav',
-    weight: 10
-  },
-  section : {
-    value : 'section',
-    weight: 10
-  },
-  summary : {
-    value : 'summary',
-    weight: 10
-  },
-  time : {
-    value : 'time',
-    weight: 10
+/**
+ * 
+ * Semantical : Builder as a wrapper IIFE for the main method
+ *
+ * @memberOf file
+ * @param {object} The cheerio library
+ * @returns {calculate} The method that returns the text with calculated markup percent value 
+ */
+const Semantical = ((cheerio) => {
+
+  const body = 'body',
+        semanticals = {
+          article: {value : 'article'},
+          aside: {value : 'aside'},
+          details : {value : 'details'},
+          figcaption : {value : 'figcaption'},
+          figure : {value : 'figure'},
+          footer : {value : 'footer'},
+          header : {value : 'header'},
+          main : {value : 'main'},
+          mark : {value : 'mark'},
+          nav : {value : 'nav'},
+          section : {value : 'section'},
+          summary : {value : 'summary'},
+          time : {value : 'time'}
+        };
+
+  let semanticalPercent = 0;
+
+  /**
+   * 
+   * calculate() : returns the text with calculated markup percent value 
+   *
+   * @memberOf file
+   * @param {string} Markup of the HTML code to be parsed
+   * @returns {string} Returns the calculation message
+   */
+  const calculate = (markup) => {
+    $ = cheerio.load(markup);
+    for (let semanticalIndex = 0, len = Object.keys(semanticals).length; semanticalIndex < len; semanticalIndex++) {
+      let elementExistance = $(body).find(semanticals[Object.keys(semanticals)[semanticalIndex]].value);
+      if (!!elementExistance && elementExistance.length > 0) {
+        semanticalPercent += (!!semanticals[Object.keys(semanticals)[semanticalIndex]]? 1 : 0);
+      }
+    };
+    return getPercentMessage($(body).length, semanticalPercent);
   }
-};
 
-for (var semanticalIndex = 0, len = Object.keys(semanticals).length; semanticalIndex < len; semanticalIndex++) {
-  var elementExistance = $('body').find(semanticals[Object.keys(semanticals)[semanticalIndex]]);
-  console.log(elementExistance);
-  if (elementExistance.length > 0) {
-    total += semanticals[Object.keys(semanticals)[semanticalIndex]].weight;
+  /**
+   * 
+   * getPercentMessage() : generates and returns the calculated text for HTML markup
+   *
+   * @memberOf file
+   * @param {number} bodyLen Markup of the HTML code to be parsed
+   * @param {number} semanticalPercent Number value of current semantical elements
+   * @returns {string} Returns the calculation message
+   */
+  const getPercentMessage = (bodyLen, semanticalPercent) => {
+    return 'Semantical as ' + ((bodyLen * semanticalPercent) / 100) + '%'
   }
-}
 
-console.log('Seamntical as ' + total + '%');
+  return {
+    calculate : calculate
+  }
+
+})(require('cheerio'));
+
+module.exports = Semantical;
